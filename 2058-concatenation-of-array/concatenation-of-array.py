fe536aaeb51d7e5fa20c39d644f508780f089ac6
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-07-08 19:26:02 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (33.81%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        arr = nums * 2
+        return arr
