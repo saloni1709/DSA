59d71c80982f75486f2a6de74b42406c7f3bd54f
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-07-10 10:39:12 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (15.44%) - LeetSync

---
+++ 742-to-lower-case/to-lower-case.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def toLowerCase(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+        if s.upper():
+            return s.lower()
+        else:
+            return s
+        
