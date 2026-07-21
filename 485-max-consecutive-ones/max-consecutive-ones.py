ae436daad63a7e25b6e76f4a237efd2b1b96e97e
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-07-21 19:43:00 +0530

    Time: 12 ms (92.55%) | Memory: 13.5 MB (53.71%) - LeetSync

---
+++ 485-max-consecutive-ones/max-consecutive-ones.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        count = 0
+        max_count = 0
+
+        for i in nums:
+            if i == 1:
+                count += 1
+            else:
+                count = 0
+                
+            if count > max_count:
+                max_count = count
+            
+        return max_count
