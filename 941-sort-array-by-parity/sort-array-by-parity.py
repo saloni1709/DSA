84d63748d2acc08eb43355b150f00113cf8a0e1c
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-07-23 21:45:57 +0530

    Time: 7 ms (16.88%) | Memory: 13.1 MB (6.68%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.py
+++ 941-sort-array-by-parity/sort-array-by-parity.py
@@ -5,29 +5,31 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        # even = []
-        # odd = []
+        even = []
+        odd = []
 
-        # for i in nums:
-        #     if i % 2 == 0:
-        #         even.append(i)
-        #     else:
-        #         odd.append(i)
-        # return even + odd
-
-
-        l = 0
-        r = len(nums) - 1
-
-        while l < r:
-            if nums[l] % 2 == 0:
-                l += 1
-            elif nums[r] % 2 != 0:
-                r -= 1
+        for i in nums:
+            if i % 2 == 0:
+                even.append(i)
             else:
-                nums[l], nums[r] = nums[r], nums[l]
-                l += 1
-                r -= 1
+                odd.append(i)
+        return even + odd
+
+
+    ## By Two Pointer :-
+
+        # l = 0
+        # r = len(nums) - 1
+
+        # while l < r:
+        #     if nums[l] % 2 == 0:
+        #         l += 1
+        #     elif nums[r] % 2 != 0:
+        #         r -= 1
+        #     else:
+        #         nums[l], nums[r] = nums[r], nums[l]
+        #         l += 1
+        #         r -= 1
         
-        return nums
+        # return nums
                 
