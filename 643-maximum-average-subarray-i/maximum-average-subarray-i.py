8ed755adf96fcd77fadd23d9185d08218dbf2cb3
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-07-25 22:46:28 +0530

    Time: 64 ms (79.59%) | Memory: 19.3 MB (17.06%) - LeetSync

---
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: float
+        """
+        
+        window_sum = 0
+
+        for i in range(k):
+            window_sum += nums[i]
+        max_sum = window_sum
+
+        for j in range(k, len(nums)):
+            window_sum -= nums[j-k]
+            window_sum += nums[j]
+
+            if window_sum > max_sum:
+                max_sum = window_sum
+
+        avg = max_sum / float(k)
+
+        return avg
