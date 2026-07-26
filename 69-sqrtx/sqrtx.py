459ac0c063c3b56d4df8414cdb071ae470deedcc
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-07-26 21:43:01 +0530

    Time: 2 ms (59.82%) | Memory: 12.5 MB (17.75%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -5,4 +5,19 @@ class Solution(object):
         :rtype: int
         """
         
-        return int(x**(0.5))
+        l = 0
+        r = x
+
+        while l <= r:
+            mid = (l+r) // 2
+
+            if (mid)*(mid) == x:
+                return mid
+            
+            elif (mid)*(mid) < x:
+                l = mid + 1
+            
+            elif (mid)*(mid) > x:
+                r = mid - 1
+        
+        return r
