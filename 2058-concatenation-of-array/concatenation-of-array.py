604ba539525cb4f39e242fa6ee904f86de1604c9
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-07-28 11:34:59 +0530

    Time: 2 ms (23.77%) | Memory: 12.6 MB (69.87%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.py
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -5,5 +5,18 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        nums = nums * 2
-        return nums
+        # nums = nums * 2
+        # return nums
+
+
+        ## By Linear Search
+
+        ans = []
+
+        for i in range(len(nums)):
+            ans.append(nums[i])
+
+        for i in range(len(nums)):
+            ans.append(nums[i])
+
+        return ans
