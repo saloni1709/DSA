5201fc7d8cd8a98d7137562219e7c33324950b47
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-07-29 13:56:59 +0530

    Time: 95 ms (73.44%) | Memory: 12.9 MB (58.22%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -15,4 +15,12 @@ class Solution(object):
                 break
         return -1
 
+        # count = 0
+        # for i in range(len(s)):
+        #     count = s.count(s[i])
+
+        #     if count == 1:
+        #         return i
+
+        # return -1
         
