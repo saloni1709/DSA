0cf757b147406366e4060e47b47730ab61438b71
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-07-30 19:25:10 +0530

    Time: 16 ms (58.62%) | Memory: 12.5 MB (91.60%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -5,16 +5,31 @@ class Solution(object):
         :rtype: bool
         """
         
-        rev = ""
+        # rev = ""
 
-        for ch in s.lower():
-            if ch.isalnum():
-                rev = rev+ch
+        # for ch in s.lower():
+        #     if ch.isalnum():
+        #         rev = rev+ch
         
-        if rev == rev[::-1]:
-            return True
-        else:
-            return False
+        # if rev == rev[::-1]:
+        #     return True
+        # else:
+        #     return False
         
+        l = 0
+        r = len(s) - 1
 
+        while l < r:
+
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+
+        return True
         
