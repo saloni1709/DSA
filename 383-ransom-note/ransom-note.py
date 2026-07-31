a352259cf2d76010c940eff21f489c71c0dc3f20
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-07-31 23:20:25 +0530

    Time: 36 ms (49.03%) | Memory: 12.6 MB (87.65%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+
+        f = {}
+        f1 = {}
+        for i in magazine:
+            f[i] = f.get(i, 0) + 1
+
+        for j in ransomNote:
+            f1[j] = f1.get(j, 0) + 1
+
+        for ch in f1:
+            if ch not in f:
+                return False
+            if f[ch] < f1[ch]:
+                return False
+        return True
+                 
+
+
