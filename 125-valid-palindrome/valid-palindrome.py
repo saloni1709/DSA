fc17284a3c8815d582f7378c9cb55aaadd4b610f
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-08-01 16:42:04 +0530

    Time: 245 ms (13.30%) | Memory: 13.1 MB (44.90%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,32 +4,33 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+
+        ans = ""
+        for ch in s:
+            if ch.isalnum():
+                ans += ch.lower()
+        if ans == ans[::-1]:
+            return True
+        else:
+            return False
         
-        # rev = ""
-
-        # for ch in s.lower():
-        #     if ch.isalnum():
-        #         rev = rev+ch
         
-        # if rev == rev[::-1]:
-        #     return True
-        # else:
-        #     return False
+
+        # l = 0
+        # r = len(s) - 1
+        # while l < r:
+
+        #     while l < r and not s[l].isalnum():
+        #         l += 1
+        #     while l < r and not s[r].isalnum():
+        #         r -= 1
+
+        #     if s[l].lower() != s[r].lower():
+        #         return False
+        #     l += 1
+        #     r -= 1
+
+        # return True
         
-        l = 0
-        r = len(s) - 1
 
-        while l < r:
-
-            while l < r and not s[l].isalnum():
-                l += 1
-            while l < r and not s[r].isalnum():
-                r -= 1
-
-            if s[l].lower() != s[r].lower():
-                return False
-            l += 1
-            r -= 1
-
-        return True
         
