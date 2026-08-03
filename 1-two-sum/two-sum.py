eb9b491949be2df8d63a5103a69d5ef8b987e741
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-08-03 12:37:25 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (82.04%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,6 +1,19 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+
+        ## Brute Force
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+
+
+        
+        f = {}
+        
+        for i, val in enumerate(nums):
+            required = target - val
+            if required in f:
+                return [f[required], i]
+            
+            f[val] = i
