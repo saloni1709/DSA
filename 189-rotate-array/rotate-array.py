4c3bb22b1813b8a21be24f519621691e470e73b9
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-08-04 23:44:33 +0530

    Time: 0 ms (100.00%) | Memory: 23.3 MB (32.65%) - LeetSync

---
+++ 189-rotate-array/rotate-array.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+
+        if len(nums) < k:
+            k = k % len(nums)
+
+        nums[:] = nums[-k:] + nums[:-k]
+        return nums
