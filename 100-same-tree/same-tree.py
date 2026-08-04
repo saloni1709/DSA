defe91b79588edc511fc825cdfe9feea47919cb5
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-08-04 11:37:16 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (22.36%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -13,13 +13,11 @@ class Solution(object):
         """
 
         if p == None and q == None:
-            return True 
+            return True
         if p == None or q == None:
             return False
-        
         if p.val != q.val:
             return False
-
-        return (self.isSameTree(p.left, q.left)) and (self.isSameTree(p.right, q.right))
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
         
