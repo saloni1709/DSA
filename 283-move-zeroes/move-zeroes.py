f8f7a076aeb1a1b4f404c923d623196b5c514a58
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-08-07 14:49:48 +0530

    Time: 890 ms (5.01%) | Memory: 13.3 MB (88.86%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -5,18 +5,18 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         
-        # for i in nums:
-        #     if i == 0:
-        #         nums.remove(i)
-        #         nums.append(i)
-        # return nums
+        for i in nums:
+            if i == 0:
+                nums.remove(i)
+                nums.append(i)
+        return nums
 
 
         ## BY TWO POINTER
 
-        l = 0
-        for r in range(len(nums)):
-            if nums[r] != 0:
-                nums[l], nums[r] = nums[r], nums[l]
-                l += 1
-        return l
+        # l = 0
+        # for r in range(len(nums)):
+        #     if nums[r] != 0:
+        #         nums[l], nums[r] = nums[r], nums[l]
+        #         l += 1
+        # return l
