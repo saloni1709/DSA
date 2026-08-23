2934bd5de32c628a8d4823853af930d20caf078f
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-08-23 17:55:49 +0530

    Time: 7 ms (81.66%) | Memory: 13.4 MB (86.86%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -14,20 +14,17 @@ class Solution(object):
         water = 0
 
         while l < r:
-            if height[l] < height[r]:   
+            if height[l] < height[r]:
                 if height[l] > max_l:
-                    max_l = height[l]
-                else:
+                    max_l = height[l]   
+                else:            
                     water += max_l - height[l]
-
                 l += 1
-            
             else:
                 if height[r] > max_r:
                     max_r = height[r]
                 else:
                     water += max_r - height[r]
-                
                 r -= 1
         
         return water
