622c3093ed65af9e2985b107c4ba3d438f9c355e
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-08-26 20:25:01 +0530

    Time: 14 ms (66.52%) | Memory: 12.5 MB (15.67%) - LeetSync

--- 374-guess-number-higher-or-lower/guess-number-higher-or-lower.py
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.py
@@ -11,18 +11,16 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        
+
         l = 0
         r = n
 
         while l <= r:
-            mid = (l + r) // 2
-
+            mid = (l + r)//2
             if guess(mid) == 0:
                 return mid
-            
             elif guess(mid) == -1:
-                r = mid-1
-            
-            elif guess(mid) == 1:
-                l = mid+1
+                r = mid - 1
+            else:
+                l = mid + 1
+        return l
