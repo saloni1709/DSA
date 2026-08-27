723380142b4aa20d5260055d604d27992fa197f0
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-08-27 21:53:14 +0530

    Time: 3 ms (58.58%) | Memory: 12.4 MB (55.18%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -7,17 +7,13 @@ class Solution(object):
         
         l = 0
         r = x
-
         while l <= r:
-            mid = (l+r) // 2
+            mid = (l+r)//2
 
-            if (mid)*(mid) == x:
+            if mid*mid == x:
                 return mid
-            
-            elif (mid)*(mid) < x:
-                l = mid + 1
-            
-            elif (mid)*(mid) > x:
-                r = mid - 1
-        
+            elif mid*mid < x:
+                l = mid+1
+            else:
+                r = mid-1
         return r
