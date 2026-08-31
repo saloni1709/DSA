d9f785097b51d3f4535514993b18606270716841
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-08-31 22:20:10 +0530

    Time: 7 ms (74.01%) | Memory: 12.3 MB (57.71%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -4,33 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-
-        value = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
+        
+        pairs = {
+            'I' : 1,
+            'V' : 5,
+            'X' : 10,
+            'L' : 50,
+            'C' : 100,
+            'D' : 500,
+            'M' : 1000
         }
 
         total = 0
 
         for i in range(len(s)):
-            curr = value[s[i]]
-            if i < len(s) - 1:
-                nxt = value[s[i+1]]
-
-                if curr < nxt:
-                    total -= curr
-                else:
-                    total += curr
-            
+            if i+1 < len(s) and pairs[s[i]] < pairs[s[i+1]]:
+                total -= pairs[s[i]]
             else:
-                total += curr
+                total += pairs[s[i]]
         
         return total
-        
-        
-        
