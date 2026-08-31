1e61dbfd73aea104b95ddd6b32db88942a24946a
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-08-31 18:13:40 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (16.20%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.py
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -7,7 +7,7 @@ class Solution(object):
         """
         
         count = 0
-        for ch in stones:
-            if ch in jewels:
+        for i in stones:
+            if i in jewels:
                 count += 1
         return count
