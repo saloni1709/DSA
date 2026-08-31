154eddec515e87c684d05bc73472d5ae5a57da1c
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-08-31 20:09:42 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (27.66%) - LeetSync

--- 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.py
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.py
@@ -4,14 +4,10 @@ class Solution(object):
         :type sentences: List[str]
         :rtype: int
         """
-
+        
         max = 0
-
         for sentence in sentences:
             word = sentence.split()
             if len(word) > max:
                 max = len(word)
-        
         return max
-        
-        
