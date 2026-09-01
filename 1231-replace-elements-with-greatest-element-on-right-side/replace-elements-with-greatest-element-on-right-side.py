0e9241956e34da88b2fdf892b8030a83926f006c
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-09-01 21:30:29 +0530

    Time: 57 ms (15.68%) | Memory: 13.8 MB (76.79%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
@@ -4,27 +4,13 @@ class Solution(object):
         :type arr: List[int]
         :rtype: List[int]
         """
-
-        # ans = []
-
-        # for i in range(0, len(arr)):
-        #     if i == len(arr)-1:
-        #         ans.append(-1)
-        #     else:
-        #         right = arr[i+1:]
-        #         ans.append(max(right))
         
-        # return ans
-
         max_right = -1
 
         for i in range(len(arr)-1, -1, -1):
             curr = arr[i]
             arr[i] = max_right
-
-            if curr > max_right:
-                max_right = curr
-        
+            max_right = max(max_right, curr)
+            
         return arr
-
         
