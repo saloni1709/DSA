b7fc826b4965b242b76e960a04b33d7092ca6982
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-09-01 18:12:25 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (19.74%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.py
+++ 414-third-maximum-number/third-maximum-number.py
@@ -4,21 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-
-        # nums = list(set(nums))
-        # for i in range(len(nums)):
-        #     nums.sort(reverse = True)
-        #     if len(nums) < 3:
-        #         return max(nums)
-        # return nums[2]
-
+        
         nums = list(set(nums))
         nums.sort()
         if len(nums) < 3:
             return nums[-1]
-        else:
-            return nums[-3]
-
-       
-
-        
+        return nums[-3]
