85d19ff243369d055def920cc9dab89e2ed58157
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-09-01 09:13:19 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (96.19%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -4,20 +4,6 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        
-        # words = s.split()
-        # s = words[::-1]
-        # s = " ".join(s)
 
-        # return s
-
-
-        ## BY STACK
-        stack = []
-        ans = ""
-        for word in s.split():
-            stack.append(word)
-        while stack:
-            ans += stack.pop() + " "
-        
-        return ans.rstrip()
+        word = s.split()[::-1]
+        return " ".join(word)
