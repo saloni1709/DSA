6cd950c71fb5acd61fbaa518b4ce108e59ce2e99
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-09-01 11:45:52 +0530

    Time: 35 ms (58.78%) | Memory: 26.6 MB (95.67%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-
+        
         ans = []
         seen = set(nums)
         for i in range(1, len(nums)+1):
