de29f73896e353e17284da6be8d0d7dc9c6f8e13
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-09-02 10:36:06 +0530

    Time: 19 ms (24.60%) | Memory: 28.1 MB (10.07%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -5,9 +5,8 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-
-        if len(nums) < k:
-            k = k % len(nums)
         
+        if len(nums) < k:
+            k = k%len(nums)
         nums[:] = nums[-k:] + nums[:-k]
         return nums
