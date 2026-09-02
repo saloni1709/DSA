6d562d1bcee3813e83c4a8e510f661acdfdeb186
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-09-02 16:15:51 +0530

    Time: 745 ms (6.07%) | Memory: 13.3 MB (97.72%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,19 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        
+
         for i in nums:
             if i == 0:
-                nums.remove(i)
-                nums.append(i)
+                nums.remove(0)
+                nums.append(0)
         return nums
-
-
-        ## BY TWO POINTER
-
-        # l = 0
-        # for r in range(len(nums)):
-        #     if nums[r] != 0:
-        #         nums[l], nums[r] = nums[r], nums[l]
-        #         l += 1
-        # return l
+        
