034a70706759e12856b627f9b083b677b2eea52a
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-09-02 17:45:51 +0530

    Time: 494 ms (86.03%) | Memory: 12.6 MB (29.20%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -14,12 +14,11 @@ class Solution(object):
 
                 if i > 0 and nums[i] == nums[i-1]:
                     continue
-
                 if j > i+1 and nums[j] == nums[j-1]:
                     continue
-
+                
                 l = j+1
-                r = len(nums) - 1
+                r = len(nums)-1
 
                 while l < r:
                     total = nums[i] + nums[j] + nums[l] + nums[r]
@@ -28,17 +27,15 @@ class Solution(object):
                         ans.append([nums[i], nums[j], nums[l], nums[r]])
                         l += 1
                         r -= 1
-
+                    
                         while l < r and nums[l] == nums[l-1]:
                             l += 1
-                        
                         while l < r and nums[r] == nums[r+1]:
                             r -= 1
-
+                    
                     elif total < target:
                         l += 1
-                    
                     else:
                         r -= 1
-
+        
         return ans
