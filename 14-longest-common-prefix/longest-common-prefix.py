dd56c27f2ac7bea2e17877363a43744c919886c5
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-09-02 21:32:06 +0530

    Time: 3 ms (27.17%) | Memory: 12.3 MB (96.64%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -8,8 +8,9 @@ class Solution(object):
         ans = ""
         for i in range(len(strs[0])):
             ch = strs[0][i]
+
             for word in strs:
-                if i >= len(word) or ch != word[i]:
+                if len(word) <= i or word[i] != ch:
                     return ans
             ans += ch
         return ans
