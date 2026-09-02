0c5a9ee904a361978eb320a73adf025dedab2625
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-09-02 22:21:51 +0530

    Time: 8 ms (53.26%) | Memory: 24.6 MB (73.00%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -10,6 +10,7 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: int
         """
+        
         if not root:
             return 0
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
