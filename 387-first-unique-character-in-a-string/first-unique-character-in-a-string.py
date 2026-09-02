18496e258f9a615ab4d6a5f60398355c91f0348f
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-09-02 17:52:45 +0530

    Time: 103 ms (57.49%) | Memory: 12.7 MB (71.76%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -4,23 +4,12 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-
-        fre = {}
-
-        for ch in s:
-            fre[ch] = fre.get(ch, 0) + 1
-        for idx, val in enumerate(s):
-            if fre[val] == 1:
-                return idx
-                break
-        return -1
-
-        # count = 0
-        # for i in range(len(s)):
-        #     count = s.count(s[i])
-
-        #     if count == 1:
-        #         return i
-
-        # return -1
         
+        f = {}
+        for i in s:
+            f[i] = f.get(i, 0) + 1
+        
+        for idx, val in enumerate(s):
+            if f[val] == 1:
+                return idx
+        return -1
