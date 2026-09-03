8cce04788a703725d9e22e3ab3b55101c1dfb140
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-09-03 19:47:07 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (99.83%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -5,21 +5,15 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-
+        
         l = 0
-        r = len(nums) - 1
-
+        r = len(nums)-1
         while l <= r:
-            mid = (l+r) // 2
-
+            mid = (l+r)//2
             if nums[mid] == target:
                 return mid
-
             elif nums[mid] < target:
-                l = mid + 1
-
-            elif nums[mid] > target:
-                r = mid - 1
-        
+                l = mid+1
+            else:
+                r = mid-1
         return l
-        
