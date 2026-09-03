9d1332f4bc1b546a1155e589226847d22a57008a
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-09-03 18:56:38 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (54.85%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -4,21 +4,16 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-
+        
         visited = set()
-
         while n != 1:
             if n in visited:
                 return False
             visited.add(n)
-
             sum = 0
-            
             while n > 0:
-                digit = n % 10
-                sum += digit*digit
-                n = n // 10
-
+                digit = n%10
+                sum += digit * digit
+                n = n//10
             n = sum
-        
         return True
