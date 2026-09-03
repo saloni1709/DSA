4783e4ac011d99c58457c5e51448dbe4fe602494
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-09-03 18:50:38 +0530

    Time: 31 ms (65.67%) | Memory: 12.7 MB (56.87%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -5,7 +5,7 @@ class Solution(object):
         :type magazine: str
         :rtype: bool
         """
-
+        
         f = {}
         f1 = {}
 
@@ -13,10 +13,9 @@ class Solution(object):
             f[i] = f.get(i, 0) + 1
         for j in ransomNote:
             f1[j] = f1.get(j, 0) + 1
-        
+
         for ch in f1:
-            if ch not in f:
-                return False
-            if f[ch] < f1[ch]:
+            if ch not in f or f[ch] < f1[ch]:
                 return False
         return True
+                
