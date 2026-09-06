a4a4fdde9b381791722f3b6128adc7908c5701fa
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-09-06 16:20:53 +0530

    Time: 51 ms (85.23%) | Memory: 19.1 MB (18.96%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -5,15 +5,16 @@ class Solution(object):
         :type k: int
         :rtype: float
         """
-
+        
         window_sum = 0
+        
         for i in range(k):
             window_sum += nums[i]
-        max_sum = window_sum 
+        max_sum = window_sum
 
         for j in range(k, len(nums)):
-            window_sum -= nums[j-k]
             window_sum += nums[j]
+            window_sum -= nums[j-k]
 
             if window_sum > max_sum:
                 max_sum = window_sum
