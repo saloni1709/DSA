3b539eb773a1b7d40310a5eac3c27bdf9e6925fb
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-09-06 19:12:25 +0530

    Time: 95 ms (81.87%) | Memory: 15.9 MB (28.37%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -6,23 +6,21 @@ class Solution(object):
         :rtype: int
         """
         
-        v = 'aeiou'
+        window_sum = 0
         count = 0
+        vowels = 'aeiou'
 
         for i in range(k):
-            if s[i] in v:
+            if s[i] in vowels:
                 count += 1
-        
         max_count = count
-
+        
         for j in range(k, len(s)):
-            if s[j-k] in v:
-                count -= 1
-            
-            if s[j] in v:
+            if s[j] in vowels:
                 count += 1
 
-            if count > max_count:
-                max_count = count
-            
+            if s[j-k] in vowels:
+                count -= 1
+            max_count = max(count, max_count)
+        
         return max_count
