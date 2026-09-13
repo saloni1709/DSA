77aa29e9acb85325165f772cdb3828c36f599b3c
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-09-13 15:45:02 +0530

    Time: 4 ms (18.01%) | Memory: 12.6 MB (22.16%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -8,13 +8,12 @@ class Solution(object):
         
         l = 0
         r = 0
-        while l < len(s) and r < len(t):
+        while l<len(s) and r<len(t):
             if s[l] == t[r]:
                 l += 1
                 r += 1
             else:
                 r += 1
-        
         if l == len(s):
             return True
         else:
