474f48c99f1ee188f6f04443a540b06828b6ca2d
Saloni Goyal <2023pietcssaloni144@poornima.org>
2026-09-13 10:16:46 +0530

    Time: 56 ms (16.24%) | Memory: 13.8 MB (76.43%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
@@ -6,11 +6,8 @@ class Solution(object):
         """
         
         max_right = -1
-
         for i in range(len(arr)-1, -1, -1):
             curr = arr[i]
             arr[i] = max_right
             max_right = max(max_right, curr)
-            
         return arr
-        
